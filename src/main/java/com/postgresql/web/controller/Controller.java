2112252b9a660249397af4972b3065a12c99d688
FelipeCarillancaDev <felipe.carillanca@gmail.com>
2024-04-21 16:24:30 -0400

    feature: se agregan las entidades ys e configura base de datos postgres

--- src/main/java/com/postgresql/web/controller/Controller.java
+++ src/main/java/com/postgresql/web/controller/Controller.java
@@ -1,15 +1,20 @@
-package com.postgresql;
+package com.postgresql.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController // define la clase como un rescontroller
 @RequestMapping("/saludar")
 public class Controller {
 
     @GetMapping("/hola")
-    public String saludar(){
+    public String saludar() {
         return "Hola mundito";
+    }
+
+    @GetMapping("chao")
+    public String despedir() {
+        return "Adios!!!";
     }
 }
